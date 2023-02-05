cfd13c214d6bdd730c0011ddcaeb025454b25b90
Vladimir Natsevsky <natsevsky@arrival.com>
2023-02-06 00:26:55 +0300

    [NEW] someproject_agreement:JIRATASK-42 Implemented a new module. It allows to manage agreements.

---
+++ someproject_agreement/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    "name": "Some Project Agreement",
+    "version": "15.0.1.0.0",
+    "category": "Extra Tools",
+    "summary": "Agreement Management",
+    "description": """
+    """,
+    "author": "Vladimir Natsevsky",
+    "depends": [
+        "base",
+        "mail",
+    ],
+    "data": [
+        "data/ir_cron.xml",
+        "data/mail_template.xml",
+        "security/agreement_security.xml",
+        "security/ir.model.access.csv",
+        "views/agreement_agreement_view.xml",
+        "views/agreement_type_view.xml",
+        "views/menuitems.xml",
+    ],
+    "demo": [
+    ],
+    "application": True,
+}
